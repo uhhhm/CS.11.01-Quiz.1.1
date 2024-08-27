ff7636e85dfe8da99bb217b41d284b9f470ae2f3
techarenz <57818506+techarenz@users.noreply.github.com>
2024-08-27 12:35:11 +0800

    Update Main.java
    Included academic honesty pledge.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+
+/**
+*****************************************************************************************************
+* By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
+* “I have neither given nor received unauthorized aid on this piece of work.”
+* NAME: 
+*****************************************************************************************************
+*/
+
 public class Main {
 
     public static void main(String[] args) {
